34aa981d311eedfdb3075c0a740c66a0cae225d7
Haider <haidrrcheema40@gmail.com>
2021-10-12 18:43:19 -0700

    I change something in this file

---
+++ connection.php
@@ -0,0 +1,35 @@
+<?php
+class DbConnection
+{
+
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'oops_login';
+
+    protected $connection;
+
+    public function __construct()
+    {
+
+        if (!isset($this->connection)) {
+
+            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+            if (!$this->connection) {
+                echo 'Cannot connect to database server';
+                exit;
+            }
+        }
+
+        return $this->connection;
+    }
+}
+
+
+?>
+
+
+
+
+
